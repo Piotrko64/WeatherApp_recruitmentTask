46ad9e18cb6fdc8d4a689b88f43a4291b4beb72f
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-07-19 21:13:09 +0200

    Modify script.js

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function addValidData(data) {
     if (data.temperatura && data.data_pomiaru && data.suma_opadu && data.cisnienie) {
         weatherInfoDiv.innerHTML = createOneLineWeatherData(data);
         checkingCityDiv.textContent = `Aktualna pogoda dla miasta: ${data.stacja}`;
+
+        addToLocalStorage(data);
     } else {
+        checkingCityDiv.textContent = "";
         weatherInfoDiv.textContent = "Coś poszło nie tak z pobraniem danych";
     }
 }
@@ -32,7 +35,9 @@ function addToLocalStorage(weatherData) {
 function showActualWeatherData(event) {
     event.preventDefault(event);
 
-    if (!selectInput.value.trim()) {
+    const valueSelect = selectInput.value;
+
+    if (!valueSelect.trim()) {
         weatherInfoDiv.textContent = "Uzupełnij najpierw poprawnie formularz!";
         return;
     }
@@ -40,15 +45,14 @@ function showActualWeatherData(event) {
     checkingCityDiv.textContent = "Ładuje...";
     weatherInfoDiv.textContent = "";
 
-    fetch(`https://danepubliczne.imgw.pl/api/data/synop/station/${selectInput.value}`)
+    fetch(`https://danepubliczne.imgw.pl/api/data/synop/station/${valueSelect}`)
         .then((data) => data.json())
+        .then((data) => {
+            addValidData(data);
+        })
         .catch((err) => {
             weatherInfoDiv.textContent = "UPS! Coś poszło nie tak!";
             console.error(err);
-        })
-        .then((data) => {
-            addValidData(data);
-            addToLocalStorage(data);
         });
 }
 
@@ -57,7 +61,7 @@ form.addEventListener("submit", showActualWeatherData);
 if (localStorage.getItem("weatherStorage")) {
     let everyResultsInLocalStorage = "";
 
-    JSON.parse(localStorage.getItem("weatherStorage")).forEach((info) => {
+    [...JSON.parse(localStorage.getItem("weatherStorage"))].reverse().forEach((info) => {
         everyResultsInLocalStorage += `<h3> ${info.stacja} </h3> ${createOneLineWeatherData(info)} `;
     });
 
